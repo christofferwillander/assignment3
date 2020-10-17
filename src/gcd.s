98bce1de08f06d0bd3d498a755ab652d43af8a80
Christoffer <christoffer.willander@gmail.com>
2020-10-17 10:01:53 +0200

    Changed to quad instructions

--- src/gcd.s
+++ src/gcd.s
@@ -8,18 +8,18 @@ main:
 	movq 	%rsi, %r9
 
 while:
-	cmp 	%r9, %r8
+	cmpq 	%r9, %r8
 	jz	end
-	cmp	%r9, %r8
+	cmpq	%r9, %r8
 	jle	else
 	jmp	if
 
 if:
-	sub	%r9, %r8
+	subq	%r9, %r8
 	jmp	while
 
 else:
-	sub	%r8, %r9
+	subq	%r8, %r9
 	jmp	while
 
 end:
