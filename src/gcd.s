c32ade7f9d202f32c4af37441d241728dd3c2e3c
Christoffer <christoffer.willander@gmail.com>
2020-10-17 13:58:22 +0200

    Fix for edge cases; if any parameter is zero, or equal

--- src/gcd.s
+++ src/gcd.s
@@ -3,22 +3,36 @@
 gcd:
 	movq 	%rdi, %r8
 	movq 	%rsi, %r9
+	cmpq	$0,	%r8
+	jz		isZero
+	cmpq	$0, %r9
+	jz		isZero
+	cmpq	%r8, %r9
+	jz		isEqual
 
 while:
 	cmpq 	%r9, %r8
-	jz	end
+	jz		end
 	cmpq	%r9, %r8
-	jle	else
-	jmp	if
+	jle		else
+	jmp		if
 
 if:
 	subq	%r9, %r8
-	jmp	while
+	jmp		while
 
 else:
 	subq	%r8, %r9
-	jmp	while
+	jmp		while
 
 end:
 	movq 	%r8, %rax
 	ret
+
+isZero:
+	movq	$0, %r8
+	movq	$0, %r9
+	jmp 	end
+
+isEqual:
+	jmp		end
