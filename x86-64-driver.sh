9911ef2d1af85df6cd032bf0571c4b0e3a4071da
Christoffer <christoffer.willander@gmail.com>
2020-10-17 10:18:37 +0200

    Minor changes.

--- x86-64-driver.sh
+++ x86-64-driver.sh
@@ -12,15 +12,29 @@ if [ $# -lt 1 ] || [ $# -gt 1 ]; then
 fi
 
 fileArg=$(echo "$1" | grep -o ".calc")
+fileName=$(echo "$1" | grep -Eo "^[^\.]+")
 
 if [ $? -eq 1 ]; then
-    echo "Invalid file type - provide a .calc file"
+    echo "ERROR: Invalid file type - provide a .calc file"
     echo $USAGE
     echo $NOTE
     exit 1
 fi
 
-fileName=$(echo "$1" | grep -Eo "^[^\.]+")
+test -f $fileName$fileArg
+
+if [ $? -eq 1 ]; then
+    echo "ERROR: Input file $fileName$fileArg does not exist"
+    exit 1
+fi
+
+test -f ./bin/calc3i.exe
+
+if [ $? -eq 1 ]; then
+    echo "ERROR: Could not find compiler file (./bin/calc3i.exe) - attempting to make"
+    make > /dev/null
+fi
+
 touch $fileName
 cat prologue > $fileName.s
 ./bin/calc3i.exe < ./$1 >> $fileName.s
