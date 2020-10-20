2c8d2f15277616cc1a40f57e64712609555b26df
elal17 <elal17@student.bth.se>
2020-10-20 13:01:48 +0200

    comments

--- src/fact.s
+++ src/fact.s
@@ -3,37 +3,37 @@
 fact:
 	pushq	%r9
 	pushq	%r8
-	xorq	%r9, %r9
+	xorq	%r9, %r9			# zeroing r9
 	movq 	%rdi, %rax
-	cqto
+	cqto						# expanding the registry
 	cmpq	$0, %rax
 	jz	zeroend
 	cmpq	$1, %rax
 	jz	zeroend
 	cmpq	$0, %rax
 	jl	negative
-    	movq 	%rax, %r8
-	subq	$1, %r8
+    movq 	%rax, %r8
+	subq	$1, %r8				# r8 is one less than rax
 	jmp	notzero
 
-notzero:
-	mulq	%r8
+notzero:						# calculate the factorial of the input values stored in rax
+	mulq	%r8					# multtply rax with r8 and store the result in rax
 	subq	$1, %r8
-	cmpq	$0, %r8
+	cmpq	$0, %r8				# if r8 is zero we have reached the end
 	jz	end
 	jmp	notzero
 
-zeroend:
+zeroend:						# if the input value is 1 or 0 it returns 1
 	movq	$1, %rax
 
 end:
-	cmpq	$1, %r9
+	cmpq	$1, %r9				# if the input value was negative
 	jz	negativeend
 	popq	%r8
 	popq	%r9
     	ret
 
-negative:
+negative:						# if the input values i negativ, we calculate the fact of the positive value and then convert to negative
 	movq	$1, %r9
 	negq	%rax
 	movq	%rax, %r8
@@ -41,7 +41,7 @@ negative:
 	jmp	notzero
 
 negativeend:
-	negq	%rax
+	negq	%rax				# adding the '-' to the return
 	popq	%r8
 	popq	%r9
 	ret
--- src/lntwo.s
+++ src/lntwo.s
@@ -1,8 +1,8 @@
 .text
 .globl lntwo
 lntwo:
-    xorq    %r10, %r10
-    movq    %rdi, %rax
+    xorq    %r10, %r10          # zero out r10
+    movq    %rdi, %rax          # move the input value to rax
     cmpq    $0, %rax
     jl      negativeCase
     cmpq    $0, %rax
@@ -11,27 +11,27 @@ lntwo:
     jz      zeroExit
     jmp     loop
 
-loop:
-    shrq    $1, %rax
+loop:                           # loop function to calculate the binary logarithm
+    shrq    $1, %rax            # shift rax to the right to divide by 2
     cmpq    $0, %rax
     jle     end
-    incq    %r10
+    incq    %r10                # r10 is used as a counter
     jmp     loop
 
-zeroExit:
+zeroExit:                       # if the input value is 1
     movq    $0, %rax
     jmp     end
 
-notExist:
-    movq    $-1, %rbx;
+notExist:                       # if the input value is 0
+    movq    $-1, %rbx;          # check value for numToASCII library
     movq    $0, %rax
     ret
 
-negativeCase:
-    movq    $-2, %rbx
+negativeCase:                   # if the input value is negative
+    movq    $-2, %rbx           # check value for numToASCII library
     movq    $0, %rax
     ret
 
-end:
+end:                            # move the result to %rax and return
     movq    %r10, %rax
     ret
