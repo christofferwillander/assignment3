4f05d7e6cab48b9025080b6adaf514062e97afaa
Christoffer <christoffer.willander@gmail.com>
2020-10-20 13:07:01 +0200

    Added comments

--- src/gcd.s
+++ src/gcd.s
@@ -3,28 +3,28 @@
 gcd:
 	pushq	%r9
 	pushq	%r8
-	movq 	%rdi, %r8
-	movq 	%rsi, %r9
-	cmpq	$0, %r8
+	movq 	%rdi, %r8	# Moving first input parameter into %r8
+	movq 	%rsi, %r9	# Moving second input parameter into %r9
+	cmpq	$0, %r8		# Checking if any of the input paramets are equal to 0
 	jz	isZero
 	cmpq	$0, %r9
 	jz	isZero
-	cmpq	%r8, %r9
+	cmpq	%r8, %r9	# Checking if input parameters are equal
 	jz	isEqual
-	cmpq	$0, %r8
+	cmpq	$0, %r8		# Checking if any input parameter is negative
 	jl	op1Neg
 	cmpq	$0, %r9
 	jl	op2Neg
 
 while:
-	cmpq 	%r9, %r8
-	jz	end
-	cmpq	%r9, %r8
+	cmpq 	%r9, %r8	# Checking if %r8, %r9 are equal - then terminate
+	jz	end		
+	cmpq	%r9, %r8	# Checking if %r8 is greater than %r9 - if so jump to if, otherwise jump to else
 	jle	else
 	jmp	if
 
 if:
-	subq	%r9, %r8
+	subq	%r9, %r8	
 	jmp	while
 
 else:
