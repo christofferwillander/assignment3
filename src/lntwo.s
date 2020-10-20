4bd0406039d1bd1df1ac4ff3067a345bb2ff8d7d
elal17 <elal17@student.bth.se>
2020-10-20 11:50:01 +0200

    lntwo function implemnted

---
+++ src/lntwo.s
@@ -0,0 +1,37 @@
+.text
+.globl lntwo
+lntwo:
+    xorq    %r10, %r10
+    movq    %rdi, %rax
+    cmpq    $0, %rax
+    jl      negativeCase
+    cmpq    $0, %rax
+    jz		notExist
+    cmpq    $1, %rax
+    jz      zeroExit
+    jmp     loop
+
+loop:
+    shrq    $1, %rax
+    cmpq    $0, %rax
+    jle     end
+    incq    %r10
+    jmp     loop
+
+zeroExit:
+    movq    $0, %rax
+    jmp     end
+
+notExist:
+    movq    $-1, %rbx;
+    pushq   $0
+    ret
+
+negativeCase:
+    movq    $-2, %rbx
+    pushq   $0
+    ret
+
+end:
+    movq    %r10, %rax
+    ret
